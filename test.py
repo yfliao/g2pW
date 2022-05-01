b82da5257bb000f06912008edc043d3c87650e2d
Yuan-Fu Liao <yfliao@mail.ntut.edu.tw>
2022-05-01 22:00:57 +0800

    remove w2s

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from paddlespeech.t2s.frontend.zh_normalization.text_normlization import TextNormalizer
 from opencc import OpenCC
 from g2pw import G2PWConverter
-from cntn import w2s
+#from cntn import w2s
 
 def run():
     tw2sp = OpenCC('tw2sp.json')
@@ -16,14 +16,14 @@ def run():
     input0 = '上校請技術人員校正FN儀器,用了25年，跟銀行借了320萬元。她出生于86年8月18日（1997/08/18），身高175.3cm'
     input1 = tw2sp.convert(input0)
     input2 = tnorm.normalize_sentence(input1)
-    input3 = w2s(input2)
-    input4 = s2twp.convert(input3)
+#    input3 = w2s(input2)
+    input4 = s2twp.convert(input2)
     output1 = g2pw(input4)
 
     print (input0)
     print (input1)
     print (input2)
-    print (input3)
+#    print (input3)
     print (input4)
     print (output1)
 
